22283ea9992a0f287feea0392ab000fd54e289e7
o-paulo <paulo.pereira707@outlook.com>
2024-12-24 03:32:26 -0300

    feat: adicionado funcao para reiniciar sorteador

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value)
     let ate = parseInt(document.getElementById('ate').value)
     
-
     let sorteados = []
     let numero
 
-    for(let i = 0; i < quantidade; i++){
+    for(let i = '0'; i < quantidade; i++){
         numero = obterNumeroAleatorio(de, ate)
         sorteados.push(numero)
 
@@ -16,11 +15,33 @@ function sortear(){
         }
 
         document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+
     }
     
-    console.log(sorteados)
+    alterarStatusBotao()
 }
 
 function obterNumeroAleatorio(min, max){
     return parseInt(Math.random() * (max - min + 1) + min)
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar')
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado')
+        botao.classList.add('container__botao')
+    } else {
+        botao.classList.remove('container__botao')
+        botao.classList.add('container__botao-desabilitado')
+    }
+}
+
+function reiniciar(){
+    let quantidade = parseInt(document.getElementById('quantidade').value = '')
+    let de = parseInt(document.getElementById('de').value = '')
+    let ate = parseInt(document.getElementById('ate').value = '')
+
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`
+    
+    alterarStatusBotao()
 }
